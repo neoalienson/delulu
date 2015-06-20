b5a02a06c92364702b471ce15551e1bdde21e2e2
Neo <neo@orz.hk>
2015-06-20 13:45:11 +0800

    storyboard draft1

---
+++ ios/dhelper/InviteHelperViewController.swift
@@ -0,0 +1,9 @@
+//
+//  InviteHelperViewController.swift
+//  dhelper
+//
+//  Created by Neo on 6/20/15.
+//  Copyright © 2015 dhelper. All rights reserved.
+//
+
+import Foundation
---
+++ ios/dhelper/MainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dhelper
+//
+//  Created by Neo on 6/20/15.
+//  Copyright © 2015 dhelper. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
