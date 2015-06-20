3e248d62653db1d7a5bef787b8a49da477b24e6e
Neo <neo@orz.hk>
2015-06-20 16:22:10 +0800

    add sign up for helper

---
+++ ios/dhelper3/HelperSignUpViewController.m
@@ -0,0 +1,37 @@
+//
+//  HelperSignUpViewController.m
+//  dhelper3
+//
+//  Created by Neo on 6/20/15.
+//  Copyright (c) 2015 dhelper3. All rights reserved.
+//
+
+#import "HelperSignUpViewController.h"
+
+@interface HelperSignUpViewController ()
+
+@end
+
+@implementation HelperSignUpViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
