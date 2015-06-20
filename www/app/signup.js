9cefdea01d58adeb4ba5565fcf6d6b286ef60706
lilian and jason <jl@lilians-MacBook-Air.local>
2015-06-20 22:51:57 +0800

    04

--- www/app/signup.js
+++ www/app/signup.js
@@ -10,8 +10,8 @@ var SignUp = React.createClass({
     getInitialState: function () {
         return {householdName: "", email: "", password: "", dhEmail: ""}
     },
-    render: function () {
-        return (
+    render: function () {        
+        return (      
             <div className="col-lg-4 col-lg-offset-4 col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
                 <div className="form-group">
                     <label for="householdName">Household name</label>
