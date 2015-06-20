51e825adbb371f83eda010cd6e349d231a6e52b5
Leonard Siu <leonard.siu@gmail.com>
2015-06-21 02:50:46 +0800

    fix household table date formatting.

--- www/app/household.js
+++ www/app/household.js
@@ -27,16 +27,16 @@ var Household = React.createClass({
                 <table className="container">
                     <thead>
                     <tr className="row">
-                        <th className="col-lg-1 col-md-1 col-sm-2 col-xs-2">Date</th>
-                        <th className="col-lg-9 col-md-9 col-sm-6 col-xs-6">Item</th>
+                        <th className="col-lg-2 col-md-2 col-sm-2 col-xs-2">Date</th>
+                        <th className="col-lg-8 col-md-8 col-sm-6 col-xs-6">Item</th>
                         <th className="col-lg-1 col-md-1 col-sm-2 col-xs-2">Amount</th>
                         <th className="col-lg-1 col-md-1 col-sm-2 col-xs-2">Region Average</th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr className="row">
-                        <td className="col-lg-1 col-md-1 col-sm-2 col-xs-2">2015-06-12</td>
-                        <td className="col-lg-9 col-md-9 col-sm-6 col-xs-6">Carrot</td>
+                        <td className="col-lg-2 col-md-2 col-sm-2 col-xs-2">2015-06-12</td>
+                        <td className="col-lg-8 col-md-8 col-sm-6 col-xs-6">Carrot</td>
                         <td className="col-lg-1 col-md-1 col-sm-2 col-xs-2">$15.99</td>
                         <td className="col-lg-1 col-md-1 col-sm-2 col-xs-2">$12.45</td>
                     </tr>
